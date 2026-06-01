77e4ddcbde9a10ab95c6fb0802acc043907926f4
Ishan Sinha <prof.ishan.sinha@gmail.com>
2026-06-01 00:05:06 -0400

    Quick fix

--- scripts/rag_pipeline.py
+++ scripts/rag_pipeline.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 import logging
